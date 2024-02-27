ce31f43b990bff2b9b1e4280a41a539b771912a8
Cynthia <cynthia-lam@outlook.com>
2024-02-27 03:45:42 -0500

    Add feedback from compass AI

--- express_server.js
+++ express_server.js
@@ -41,6 +41,6 @@ app.get("/set", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
