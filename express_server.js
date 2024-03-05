db30c2e4adb2c3ba823392f4ef3fab2eae09a0dc
Cynthia <cynthia-lam@outlook.com>
2024-03-04 22:47:03 -0500

    Move all headers to _header partial

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -15,7 +15,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 function generateRandomString() {
   let result = "";
@@ -87,23 +89,27 @@ app.get("/set", (req, res) => {
  /******************************************************************** 
  Apply ejs template files
 **********************************************************************/
- // index
+ // Main page
  app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+                          username: req.cookies["username"] };
+                          // console.log(req.cookies["username"]);
   res.render("urls_index", templateVars);
 });
 
-// new URLs
+// New URL page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
-// showing details of one short -> long URL
+// Show page
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id,
+                        longURL: urlDatabase[req.params.id],
+                        username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
-
 
 // Redirect u/shortURL to the longURL
 app.get("/u/:id", (req, res) => {
