2cdff0d39ac23fc530b895f8b425c861f59f9b68
Cynthia <cynthia-lam@outlook.com>
2024-03-09 01:18:18 -0500

    Add test directory and add 2 mocha tests for getUserByEmail

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@
 const getUserByEmail = function(email, database) {
   for (const user in database) {
     if (database[user].email === email) {
-      return database[user];
+      return database[user].id;
     }
   }
   return;
---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    console.log("user: ", user);
+    console.log("expectedUserID: " ,expectedUserID);
+    assert.equal(expectedUserID, user);
+  });
+
+  it('should return underfined with invalid email', function() {
+    const user = getUserByEmail("notrealemail@web.com", testUsers)
+    const expectedUserID = undefined;
+    assert.equal(user, expectedUserID);
+  });
+});
