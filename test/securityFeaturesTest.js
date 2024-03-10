f48ce4530b6a6c9c8f62e9a58b1793ecd8dfdf07
Cynthia <cynthia-lam@outlook.com>
2024-03-09 20:04:21 -0500

    Add 4 tests from LLM to test redirects and appropriate status codes

--- test/securityFeaturesTest.js
+++ test/securityFeaturesTest.js
@@ -7,6 +7,7 @@ chai.use(chaiHttp);
 
 describe('URL Shortener App', function() {
   let agent;
+
   beforeEach(function() {
     agent = chai.request.agent(appUrl);
   });
@@ -22,6 +23,32 @@ describe('URL Shortener App', function() {
         assert.isTrue(res.redirect);
         assert.match(res.headers.location, /login/);
         assert.equal(res.status, 302);
+      });
+  });
+
+  it('GET /urls/new should redirect to /login with status code 302 if the user is not logged in', function() {
+    return agent
+      .get('/urls/new').redirects(0)
+      .end((err, res, body) => {
+        assert.isTrue(res.redirect);
+        assert.match(res.headers.location, /login/);
+        assert.equal(res.status, 302);
+      });
+  });
+  
+  it('GET /urls/NOTEXISTS should return 404', function() {
+    return agent
+      .get('/urls/NOTEXISTS').redirects(0)
+      .then(function(res) {
+        assert.equal(res.status, 404);
+      });
+  });
+  
+  it('GET /urls/b2xVn2 should return 403', function() { //////still doesnt work
+    return agent
+      .get('/urls/b2xVn2').redirects(0)
+      .then(function(res) {
+        assert.equal(res.status, 403);
       });
   });
 });
